d88b36b9f2db9d69b31b64ab6629e5bb9ff31106
Jason Lengstorf <jason@lengstorf.com>
2020-10-01 10:56:08 -0700

    feat: add client-side webmention support

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-import Layout from '@components/Layout'
-import PostList from '@components/PostList'
+import { useEffect, useState } from 'react';
+import Layout from '@components/Layout';
+import PostList from '@components/PostList';
 
-import getPosts from '@utils/getPosts'
+import getPosts from '@utils/getPosts';
 
 const Index = ({ posts, title, description, ...props }) => {
+  const [mentions, setMentions] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://webmention.io/api/mentions.jf2?domain=next-netlify-webmention.com&token=LrmFEWB2_3UWptWP5wjK3Q'
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setMentions(result.children);
+      });
+  }, []);
   return (
     <>
       <Layout pageTitle={title} description={description}>
@@ -24,6 +35,28 @@ const Index = ({ posts, title, description, ...props }) => {
           , and a tutorial on how to build it {` `}
           <a href="https://url.netlify.com/ByVW0bCF8">here</a>.
         </p>
+        <div>
+          <h2>Discussion about this site on the web</h2>
+          <p>This uses Webmention</p>
+          {mentions.map((mention) => (
+            <div
+              style={{
+                display: 'grid',
+                gap: '1rem',
+                gridTemplateColumns: '100px 1fr',
+              }}
+            >
+              <a href={mention.author.url}>
+                <img
+                  style={{ width: 100 }}
+                  src={mention.author.photo}
+                  alt={mention.author.name}
+                />
+              </a>
+              <div dangerouslySetInnerHTML={{ __html: mention.content.html }} />
+            </div>
+          ))}
+        </div>
       </Layout>
       <style jsx>{`
         .title {
@@ -32,17 +65,17 @@ const Index = ({ posts, title, description, ...props }) => {
         }
       `}</style>
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
 export async function getStaticProps() {
-  const configData = await import(`../siteconfig.json`)
+  const configData = await import(`../siteconfig.json`);
 
   const posts = ((context) => {
-    return getPosts(context)
-  })(require.context('../posts', true, /\.md$/))
+    return getPosts(context);
+  })(require.context('../posts', true, /\.md$/));
 
   return {
     props: {
@@ -50,5 +83,5 @@ export async function getStaticProps() {
       title: configData.default.title,
       description: configData.default.description,
     },
-  }
+  };
 }
